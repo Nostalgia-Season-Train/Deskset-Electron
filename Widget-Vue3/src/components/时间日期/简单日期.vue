2db616a7e4b3cd14ce9b28f74a81cf137335b583
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2024-12-24 19:53:43 +0800

    日期时间：简单日期、简单时间

---
+++ Widget-Vue3/src/components/时间日期/简单日期.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from "vue"
+import axios from "axios"
+
+const year  = ref("2024")
+const month = ref("12")
+const day   = ref("24")
+const week  = ref("星期天")
+
+const date = async () => {
+  const response_date = await axios.get("http://127.0.0.1:8000/v0/datetime/date")
+  year.value = response_date.data.data.year
+  month.value = response_date.data.data.month
+  day.value = response_date.data.data.day
+
+  const response_week = await axios.get("http://127.0.0.1:8000/v0/datetime/week")
+  week.value = response_week.data.data.week
+}
+date()
+
+
+import { useIntervalFn } from '@vueuse/core'
+
+useIntervalFn(date, 60000)
+</script>
+
+
+<template>
+  <div class="date" v-widget-drag>
+    <span class="year-month-day">{{ year }} 年 {{ month }} 月 {{ day }} 日</span>
+    <span class="split"> - </span>
+    <span class="week">{{ week }}</span>
+  </div>
+</template>
+
+
+<style scoped>
+.date {
+  color: white;
+}
+
+.year-month-day {
+  font-size: 20px;
+}
+
+.split, .week {
+  font-size: 16px;
+  color: rgba(255, 255, 255, 0.7);
+}
+</style>
