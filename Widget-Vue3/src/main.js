0ed2a5b957a66460e32a3f27b497f7a7630e9130
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2024-12-24 19:45:57 +0800

    引入 vueuse、echarts；Element Plus 使用中文 - 修改：Widget-Vue3/package.json - 修改：Widget-Vue3/src/main.js

--- Widget-Vue3/src/main.js
+++ Widget-Vue3/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 
 import ElementPlus from 'element-plus'
+import zh_CN from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+
+import 'echarts'
 
 import Desktop from './Desktop.vue'
 import drag from './global/widgetDrag'
@@ -10,6 +13,6 @@ import drag from './global/widgetDrag'
 
 const app = createApp(Desktop)
 
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zh_CN })
 app.use(drag)
 app.mount('#desktop')
