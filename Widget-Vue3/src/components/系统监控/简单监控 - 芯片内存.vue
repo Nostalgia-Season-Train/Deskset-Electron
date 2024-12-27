d76fdde2b34c2687c14b4f939a267a2ac07e2bca
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2024-12-27 16:46:02 +0800

    简单监控：芯片内存 - 新增：Widget-Vue3/src/components/系统监控/简单监控 - 芯片内存.vue

---
+++ Widget-Vue3/src/components/系统监控/简单监控 - 芯片内存.vue
@@ -0,0 +1,90 @@
+<script setup>
+import VChart from "vue-echarts"
+import * as echarts from "echarts"
+
+const option = {
+  animation: false,
+  color: ["#FFFFFF"],
+  // bottom 5 把线抬起来
+  grid: { top: 0, bottom: 5, left: 0, right: 0 },
+  xAxis: {
+    show: false,
+    type: "category"
+  },
+  yAxis: {
+    max: 100, min: 0,
+    show: false
+  },
+  series: {
+    data: Array(40).fill(0),
+    type: "line",
+    showSymbol: false,
+    lineStyle: { width: 1 },
+    areaStyle: {
+      opacity: 0.8,
+      color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+        { offset: 0, color: "rgba(255, 255, 255, 1)" },
+        { offset: 1, color: "rgba(255, 255, 255, 0)" }
+      ])
+    }
+  }
+}
+
+
+import { ref } from "vue"
+import axios from "axios"
+
+const cpu_percent = ref(0)
+const optionCPU = ref(structuredClone(option))
+
+const memory_percent = ref(0)
+const optionRAM = ref(structuredClone(option))
+
+const device = async () => {
+  const response_cpu = await axios.get("http://127.0.0.1:8000/v0/device/cpu")
+  cpu_percent.value = response_cpu.data.data.percent
+  optionCPU.value.series.data.shift()
+  optionCPU.value.series.data.push(response_cpu.data.data.percent)
+
+  const response_memory = await axios.get("http://127.0.0.1:8000/v0/device/memory")
+  memory_percent.value = response_memory.data.data.percent
+  optionRAM.value.series.data.shift()
+  optionRAM.value.series.data.push(response_memory.data.data.percent)
+}
+device()
+
+
+import { useIntervalFn } from "@vueuse/core"
+
+useIntervalFn(device, 1000)
+</script>
+
+
+<template>
+  <div class="device">
+    <div class="cpu" v-widget-drag>
+      <!-- pointer-events: none 不需要与图表交互 -->
+      <div style="width: 250px; height: 70px; pointer-events: none;">
+        <v-chart :option="optionCPU" />
+      </div>
+      <span class="text">CPU</span>
+      <span class="split">&nbsp;</span>
+      <span class="cpu-num">{{ cpu_percent }}%</span>
+    </div>
+    <div class="memory" v-widget-drag>
+      <div style="width: 250px; height: 70px; pointer-events: none;">
+        <v-chart :option="optionRAM" />
+      </div>
+      <span class="text">RAM</span>
+      <span class="split">&nbsp;</span>
+      <span class="num">{{ memory_percent }}%</span>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.device {
+  color: white;
+}
+</style>
