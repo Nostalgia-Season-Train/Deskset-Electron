eb66a93b698217c2dbf2481240454919cb1c4c04
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2024-12-30 15:07:13 +0800

    简单搜索：基本样式

---
+++ Widget-Vue3/src/components/搜索/简单搜索.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue"
+import { Search } from "@element-plus/icons-vue"
+
+const input = ref("")
+
+const refresh = () => {
+  console.log(input.value)
+}
+</script>
+
+
+<template>
+  <div class="search" v-widget-drag>
+    <el-input v-model="input" :prefix-icon="Search" placeholder="搜索"
+      style="width: 320px; height: 45px;"
+      @input="refresh"
+    />
+  </div>
+</template>
