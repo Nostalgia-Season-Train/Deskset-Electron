59cecb402a26093c3f9d6d850cae4708e592ff9f
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-14 17:16:50 +0800

    Electron：优化窗口置底，添加 0x52C 指令

--- src-application/module_C/main.c
+++ src-application/module_C/main.c
@@ -8,15 +8,15 @@ BOOL CALLBACK findDefView(HWND hWnd, LPARAM lParam) {
   WCHAR lpClassName[256];
   GetClassNameW(hWnd, lpClassName, 256);
 
-  // 2、判断窗口类名，是不是 Progman 或 WorkerW
-  if (wcscmp(lpClassName, L"Progman") == 0 || wcscmp(lpClassName, L"WorkerW") == 0) {
+  // 2、判断窗口类名，是不是 WorkerW
+  if (wcscmp(lpClassName, L"WorkerW") == 0) {
     // 3、如果是，查找 DefView
     HWND hDefView = FindWindowExW(hWnd, 0, L"SHELLDLL_DefView", 0);
 
     // 4、如果找到，返回数据并结束遍历
     if (hDefView != NULL) {
       HWND *pReturn = (HWND *)lParam;
-      *pReturn = hDefView;
+      *pReturn = hWnd;
       return FALSE;
     }
   }
@@ -24,27 +24,37 @@ BOOL CALLBACK findDefView(HWND hWnd, LPARAM lParam) {
   return TRUE;
 }
 
-HWND getDefView() {
-  HWND hDefView = NULL;
+HWND getWorkerW() {
+  HWND hWorkerW = NULL;
 
-  EnumWindows(findDefView, (LPARAM)&hDefView);
+  EnumWindows(findDefView, (LPARAM)&hWorkerW);  // 找到有 DefView 的 WorkerW 窗口
 
-  return hDefView;
+  return hWorkerW;
+}
+
+void send0x52C() {
+  SendMessageTimeoutW(FindWindowW(L"Progman", 0), 0x52C, 0, 0, 0, 100, 0);
 }
 
 int main(int handleElectron, int howToKeepOnDesktop) {
   HWND hElectron = (HWND)(uintptr_t)handleElectron;
 
+  // 第一步：发送 0x52C 指令
+  send0x52C();
+
+  // 第二步：按照 howToKeepOnDesktop 选择窗口置底方案
   switch (howToKeepOnDesktop) {
     case 0:
-      HWND hDefView = getDefView();
-      if (hDefView != NULL) {
-        SetParent(hElectron, hDefView);
+      // 方案一：设为 WorkerW 窗口的子窗口
+      HWND hWorkerW = getWorkerW();
+      if (hWorkerW != NULL) {
+        SetParent(hElectron, hWorkerW);
         break;
       } else {
         return 1001;
       }
     case 1:
+      // 方案二：z-index 置底
       SetWindowPos(hElectron, HWND_BOTTOM, 0, 0, 0, 0,
                    SWP_NOSIZE |
                    SWP_NOMOVE |
