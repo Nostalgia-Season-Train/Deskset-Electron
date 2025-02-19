a09dfc2b19c8e5ede6ab457508ce989029522598
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-20 01:05:32 +0800

    组件：添加新组件 回收站

---
+++ src-components/快速启动/回收站.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Delete } from '@element-plus/icons-vue'
+import { desksetReq } from '../request'
+
+const open_recycle = () => {
+  desksetReq.get('/v0/quick/open-recycle')
+}
+</script>
+
+
+<template>
+<div class="button" @click="open_recycle">
+  <el-icon size="30px" style="color: white;"><Delete /></el-icon>
+</div>
+</template>
+
+
+<style scoped>
+.button {
+  display: flex;
+  background-color: #FFFFFF01;
+}
+.button:active {
+  background-color: rgba(158, 158, 158, 0.5);
+}
+</style>
